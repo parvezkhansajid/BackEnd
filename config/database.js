8e7da47d9aea55877bade414857a671e35d6de2d
parvezkhansajid <parvezkhansajid@gmail.com>
2024-04-07 23:40:55 +0600

    Handled Errors

--- config/database.js
+++ config/database.js
@@ -1,14 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = () => {
-  mongoose
-    .connect(process.env.DB_URI)
-    .then((data) => {
-      console.log(`Database connected to: ${data.connection.host}`);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  mongoose.connect(process.env.DB_URI).then((data) => {
+    console.log(`Database connected to: ${data.connection.host}`);
+  });
 };
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const port = process.env.PORT;
 
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
 const connectDB = require("./config/database");
 connectDB();
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
